1013b14ccb03ac0980d7dbb0933018b7f4ffd614
Luc-Frederic Langis <luc-frederic.langis@ekino.com>
2017-07-29 18:28:51 +0200

    refactor(scripts)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,6 +37,13 @@ public class Player : MonoBehaviour
 
 	Controller2D controller;
 
+	Vector2 directionalInput;
+	bool wallSliding;
+	int wallDirX;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
 	void Start ()
 	{
 		controller = GetComponent<Controller2D> ();
@@ -52,18 +59,85 @@ public class Player : MonoBehaviour
 		print ("Gravity: " + gravity + " Jump Velocity: " + maxJumpVelocity);
 	}
 
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update ()
 	{
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 
+		CalculateVelocity ();
+
+		HandleWallSliding ();
+
+		controller.Move (velocity * Time.deltaTime, directionalInput);
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+	}
+
+
+	/// <summary>
+	/// Sets the directional input.
+	/// </summary>
+	/// <returns>The directional input.</returns>
+	/// <param name="Input">Input.</param>
+	public void SetDirectionalInput (Vector2 Input)
+	{
+		directionalInput = Input;
+	}
+
+	/// <summary>
+	/// Raises the jump input down event.
+	/// </summary>
+	public void OnJumpInputDown ()
+	{
+
+		if (wallSliding) {
+			// trying to move on the same direction of the input
+			if (wallDirX == directionalInput.x) {
+				velocity.x = -wallDirX * wallJumpClimb.x;
+				velocity.y = wallJumpClimb.y;
+			}
+				// ain't touching the direction controller
+				else if (directionalInput.x == 0) {
+				velocity.x = -wallDirX * wallJumpOff.x;
+				velocity.y = wallJumpOff.y;
+			}
+				// input is opposite direction
+				else {
+				velocity.x = -wallDirX * wallLeap.x;
+				velocity.y = wallLeap.y;
+			}
+		}
+
+		if (controller.collisions.below) {
+			velocity.y = maxJumpVelocity;
+		}
+		
+	}
+
+	/// <summary>
+	/// Raises the jump input up event.
+	/// </summary>
+	public void OnJumpInputUp ()
+	{
+		if (velocity.y > minJumpVelocity) {
+			velocity.y = minJumpVelocity;
+		}
+	}
+
+	/// <summary>
+	/// Handles the wall sliding.
+	/// </summary>
+	/// <returns>The wall sliding.</returns>
+	void HandleWallSliding ()
+	{
 		// 1 wall left -1 wall right
-		int wallDirX = (controller.collisions.left) ? -1 : 1;
-
-		float targetVelocityX = input.x * moveSpeed;
-		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+		wallDirX = (controller.collisions.left) ? -1 : 1;
 
 		// wall jump
-		bool wallSliding = false;
+		wallSliding = false;
 		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
 			wallSliding = true;
 
@@ -76,7 +150,7 @@ public class Player : MonoBehaviour
 				velocityXSmoothing = 0;
 				velocity.x = 0;
 
-				if (input.x != wallDirX && input.x != 0) {
+				if (directionalInput.x != wallDirX && directionalInput.x != 0) {
 					timeToWallUnstick -= Time.deltaTime;
 				} else {
 					timeToWallUnstick = wallStickTime;
@@ -86,45 +160,17 @@ public class Player : MonoBehaviour
 
 			}
 		}
+		
+	}
 
-		if (Input.GetButtonDown ("Jump")) {
-
-			if (wallSliding) {
-				// trying to move on the same direction of the input
-				if (wallDirX == input.x) {
-					velocity.x = -wallDirX * wallJumpClimb.x;
-					velocity.y = wallJumpClimb.y;
-				}
-				// ain't touching the direction controller
-				else if (input.x == 0) {
-					velocity.x = -wallDirX * wallJumpOff.x;
-					velocity.y = wallJumpOff.y;
-				}
-				// input is opposite direction
-				else {
-					velocity.x = -wallDirX * wallLeap.x;
-					velocity.y = wallLeap.y;
-				}
-			}
-
-			if (controller.collisions.below) {
-				velocity.y = maxJumpVelocity;
-			}
-		}
-
-		if (Input.GetButtonUp ("Jump")) {
-			if (velocity.y > minJumpVelocity) {
-				velocity.y = minJumpVelocity;
-			}
-		}
-
-
-		// gravity * time in deconds it tool to complete the last frame
+	/// <summary>
+	/// Calculates the velocity.
+	/// </summary>
+	/// <returns>The velocity.</returns>
+	void CalculateVelocity ()
+	{
+		float targetVelocityX = directionalInput.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
 		velocity.y += gravity * Time.deltaTime;
-		controller.Move (velocity * Time.deltaTime, input);
-
-		if (controller.collisions.above || controller.collisions.below) {
-			velocity.y = 0;
-		}
 	}
 }
